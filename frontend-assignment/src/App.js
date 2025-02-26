c8e1157d1203e611c938f6e1c9fdb76fa6c7040d
Gourhari Sharma <gourharisharma@Gourharis-MacBook-Air.local>
2025-02-26 17:27:17 +0530

    initialized react project

---
+++ frontend-assignment/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/styles.css";
+import LandingPage from "./components/LandingPage";
+
+function App() {
+  return <LandingPage />;
+}
+
+export default App;
